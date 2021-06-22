22f6c5fc5a305978d2ea3c66ce3f47c36f68b1a0
bitcamp <bitcamp@DESKTOP-TGVBFKE>
2021-06-22 18:17:30 +0900

    공고 목록 경로 수정

--- FinalProject/src/main/java/job/data/gonggo/GonggoContoroller.java
+++ FinalProject/src/main/java/job/data/gonggo/GonggoContoroller.java
@@ -3,6 +3,7 @@ package job.data.gonggo;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
