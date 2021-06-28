4041a6160ca6a9095492c958fdec0287bf99c801
DESKTOP-TGVBFKE\bitcamp <omg_min@naver.com>
2021-06-28 10:08:23 +0900

    import 삭제

--- FinalProject/src/main/java/job/data/admin/AdminHomeController.java
+++ FinalProject/src/main/java/job/data/admin/AdminHomeController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import admin.data.AdminMapper;
 
 @Controller
 public class AdminHomeController {
