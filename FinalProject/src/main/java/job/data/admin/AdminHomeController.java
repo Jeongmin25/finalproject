b08a61125ac6ea7dc0f16c1b41a9bcf908c253d2
coco99827 <coco99827@gmail.com>
2021-06-30 11:52:47 +0900

    관리자페이지공지관리폴더추가-추가,수정,삭제구현,어드민홈컨트롤러 매핑 수정

--- FinalProject/src/main/java/job/data/admin/AdminHomeController.java
+++ FinalProject/src/main/java/job/data/admin/AdminHomeController.java
@@ -13,48 +13,72 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-
+import job.data.admin.AdminMapper;
 
 @Controller
 public class AdminHomeController {
 	  @Autowired
 	   AdminMapper mapper;
 	  
-	  @GetMapping({"/admin2"})
+	  @GetMapping({"/adminmain","/adminlogin"})
 		public String main()
 		{
-			return "index";
+			return "/adminlogin/loginform";
 		}
-	  
 	 
-	  @GetMapping("/login/loginmain")
-		public String login()
+		
+		@PostMapping("/adminloginprocess")
+		public String loginCheck(@RequestParam String id,
+				@RequestParam String pass,HttpSession session)
 		{
-			return "login/loginform";
+			int n=mapper.loginCheck(id, pass);
+			if(n==1)
+			{
+				//세션 저장(2개)
+				session.setAttribute("loginok","yes");
+				session.setAttribute("myid",id);
+				//메인페이지로 리다이렉트
+				return "redirect:adminmain";
+			}else {
+				//loginfail.jsp로 포워드
+				return "/adminlogin/loginfail";
+				
+			}
+			
 		}
+		
+		@GetMapping({"/adminlogout"})
+		public String logout(HttpSession session)
+		{
+			session.removeAttribute("loginok");
+			session.removeAttribute("myid");
+			return "/adminlogin/loginform";
+			
+		}
+		
 	  
-	  @GetMapping("/member/list")
+	  @GetMapping("/adminmember/list")
 		public String member()
 		{
-			return "member/list";
+			return "/adminmember/list";
 		}
 
-	  @GetMapping("/payment/list")
+	  @GetMapping("/adminpayment/list")
 		public String payment()
 		{
-			return "payment/list";
+			return "/adminpayment/list";
 		}
 	  
-	  @GetMapping("/jobnotice/list")
+	  @GetMapping("/adminjobnotice/list")
 		public String jobnotice()
 		{
-			return "jobnotice/list";
+			return "/adminjobnotice/list";
 		}
 	  
-	  @GetMapping("/empreview/list")
+	  @GetMapping("/adminempreview/list")
 		public String empreview()
 		{
-			return "empreview/list";
+			return "/adminempreview/list";
 		}
 	  
 	  
