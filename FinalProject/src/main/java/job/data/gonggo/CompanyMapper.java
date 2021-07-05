9d7039f511e1004f80b4aac5992efa10fda086c5
susu9655 <rlatngus9655@gmail.com>
2021-07-05 15:32:37 +0900

    북마크, 지원하기 기능 구현중

--- FinalProject/src/main/java/job/data/gonggo/CompanyMapper.java
+++ FinalProject/src/main/java/job/data/gonggo/CompanyMapper.java
@@ -1,6 +1,7 @@
 package job.data.gonggo;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -18,6 +19,22 @@ public interface CompanyMapper {
 	
 	public void updateGonggo(CompanyDto dto);
 	public void updateCategory(CategoryDto dto);
+	
+	//bookmark되어있는지 확인
+	public int searchNum(Map<String, String>map);
+	//bookmark값 삽입
+	public void insertBookmark(Map<String, String>map);
+	//bookmark 삭제
+	public void deleteBookmark(Map<String, String>map);
+	//북마크한 총 개수
+	public int getCountOfBookmark(String id);
+	//개인별 북마크한 값 가져오기
+	public List<CompanyDto> getListOfCompany(String id);
+	
+	//apply 삽입
+	//public void insertApply(ApplyDto dto);
+	//apply 개수
+	public int getCountOfApply(String id);
 }
 
 
