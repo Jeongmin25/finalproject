75c732a0357366f34113638dbe2e32a10d494c3f
kimkyounghee32 <rlarudgml_32@naver.com>
2021-07-06 16:06:57 +0900

    채용공고 리스트 출력

--- FinalProject/src/main/java/job/data/gonggo/GonggoContoroller.java
+++ FinalProject/src/main/java/job/data/gonggo/GonggoContoroller.java
@@ -253,5 +253,26 @@ public class GonggoContoroller {
 		   return mv;
 	   }
 	   
+	   
+	   @GetMapping("/pjlist")
+	   public ModelAndView pjlist() {
+	      ModelAndView mview =new ModelAndView();
+	      //목록 가져오기
+	      List<CompanyDto> gonggolist=mapper.getAlldatas();
+	      Date date=new Date();
+          long time= date.getTime();
+          
+          mview.addObject("date", date);
+          mview.addObject("time",time);
+	      mview.addObject("gonggolist",gonggolist);
+	      
+	      for(CompanyDto d:gonggolist)
+	      
+	      mview.setViewName("/pjlist/list");
+	      return mview;
+	   }
+	   
+
+	   
 
 }
--- FinalProject/src/main/java/job/data/pjlist/Listcontroller.java
+++ FinalProject/src/main/java/job/data/pjlist/Listcontroller.java
@@ -1,21 +1,9 @@
 package job.data.pjlist;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.multipart.MultipartFile;
 
-import job.data.gonggo.CategoryDto;
-import job.data.gonggo.CompanyDto;
 import job.data.gonggo.CompanyMapper;
 
 @Controller
@@ -25,47 +13,11 @@ public class Listcontroller {
 	@Autowired
 	CompanyMapper mapper;
 	
-	@GetMapping("/pjlist")
+	@GetMapping("/pjlist/test")
 	public String index(){
 		
 		return "/pjlist/list";	
 	}
 	
-	
-	@PostMapping("/pjlist/test")
-	   public String pjListTest(
-			   @ModelAttribute CompanyDto dto,
-			   @ModelAttribute CategoryDto category,
-			   HttpServletRequest request)
-	   {
-		   String path=request.getSession().getServletContext().getRealPath("/gonggophoto"); 
-		   System.out.println(path); 
-		   
-		   SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmmss");
-		   String fileName="photo"+sdf.format(new Date())+"_"+dto.getUpload().getOriginalFilename();
-		   //파일명= "photo"+날짜(년월일시분초)+dto에 업로드된 실제 파일이름
-		   dto.setEmpimg(fileName);
-		   MultipartFile uploadFile=dto.getUpload();
-		   try { 
-				 uploadFile.transferTo(new File(path+"\\"+fileName));
-				} catch(IllegalStateException | IOException e) { 
-					e.printStackTrace(); } 
-		   
-		   //db insert 
-			mapper.insertGonggo(dto);
-			int num=mapper.getInsertNum();
-			category.setNum(num);
-			
-			String ctg[]=category.getCtg().split(",",-1);
-			String tag[]=category.getTag().split(",",-1);
-			
-			for(int i=0; i<ctg.length; i++) {
-				category.setCtg(ctg[i]);
-				category.setTag(tag[i]);
-				mapper.insertCategory(category);
-			}
-
-			return "/pjlist/list"; 
-	   }
 
 }
